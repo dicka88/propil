451b3bbe23c7aeb5add77b118ee648a53074e62a
Dicka Ismaji <dickaismaji@gmail.com>
2022-04-16 14:11:13 +0700

    feat: add registerConfig
    and add  proptypes

--- components/Input/index.jsx
+++ components/Input/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-export default function Input({ className, isError, name, register, ...rest }) {
+export default function Input({ className, isError, name, register, registerConfig, ...rest }) {
   return (
     <input
-      {...(register && register(name))}
-      className={classNames("bg-gray-100 w-full py-2 px-2 rounded-lg resize-none focus:ring-black focus:outline-black focus:border-black", {
+      {...(register && register(name, registerConfig))}
+      className={classNames("bg-gray-100 w-full py-2 px-2 rounded-lg resize-none focus:ring-black focus:outline-black focus:border-black", className, {
         "border border-red-500": isError
       })}
       {...rest}
@@ -16,5 +16,8 @@ export default function Input({ className, isError, name, register, ...rest }) {
 
 Input.propTypes = {
   className: PropTypes.string,
-  isError: PropTypes.bool
+  isError: PropTypes.bool,
+  name: PropTypes.string,
+  register: PropTypes.any,
+  registerConfig: PropTypes.object
 };
