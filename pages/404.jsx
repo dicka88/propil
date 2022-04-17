89774b1d7e7dbb7179a29746a9e4874992561697
Dicka Ismaji <dickaismaji@gmail.com>
2022-04-17 12:25:29 +0700

    feat: add meta tag title on 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -5,12 +6,15 @@ import React from 'react';
 function Error404() {
   return (
     <div className="container">
+      <Head>
+        <title>404 Page Not Found</title>
+      </Head>
       <div className="flex justify-center items-center h-screen">
         <div className="text-center">
           <div className="relative w-[350px] h-[350px] mb-8">
             <Image src="/flame-error.png" layout="fill" objectFit="contain" />
           </div>
-          <h1 className="font-light mb-8 text-gray-500">Seem the page is not existed</h1>
+          <h1 className="font-light mb-8 text-gray-500">Look like the url is wrong</h1>
           <Link href="/">
             <a>
               <button className="bg-black text-white py-2 px-6 rounded-md">Make your own resume</button>
